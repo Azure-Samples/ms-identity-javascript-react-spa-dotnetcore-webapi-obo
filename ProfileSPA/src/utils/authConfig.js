1f197582d4e9e29c824c61fa1434a1afb5d6685a
derisen <dogan.erisen@gmail.com>
2020-05-02 16:13:56 -0700

    masking config params

--- ProfileSPA/src/utils/authConfig.js
+++ ProfileSPA/src/utils/authConfig.js
@@ -4,7 +4,7 @@ import { UserAgentApplication } from "msal";
 // visit https://azuread.github.io/microsoft-authentication-library-for-js/docs/msal/modules/_authenticationparameters_.html
 export const msalApp = new UserAgentApplication({
     auth: {
-        clientId: "c33a6b4b-a79b-432b-bd15-e4900976c2ab",
+        clientId: "Enter the Client Id (aka 'Application ID')",
         authority: "https://login.microsoftonline.com/consumers",
         validateAuthority: true,
         redirectUri: "http://localhost:3000",
@@ -18,8 +18,8 @@ export const msalApp = new UserAgentApplication({
 
 // Coordinates and required scopes for your web api
 export const apiConfig = {
-    resourceUri: "https://localhost:44351/api/profile/",
-    resourceScope: "https://msaltestingjs.onmicrosoft.com/TodoListClient-and-Service/access_as_user"
+    resourceUri: "Enter the TodoList Web APIs base address, e.g. 'https://localhost:44351/api/todolist/'",
+    resourceScope: "Enter the API scopes as declared in the app registration 'Expose an Api' blade in the form of 'api://{clientId}/access_as_user'"
 }
 
 /** 
