39254f50b7e229360345c116f80bf449e36198a7
derisen <dogan.erisen@gmail.com>
2020-05-19 19:49:30 -0700

    minor scripts correction

--- AppCreationScripts/Configure.ps1
+++ AppCreationScripts/Configure.ps1
@@ -402,7 +402,7 @@ Function ConfigureApplications
    # Update config file for 'client'
    $configFile = $pwd.Path + "\..\ProfileSPA\src\utils\authConfig.js"
    Write-Host "Updating the sample code ($configFile)"
-   $dictionary = @{ "Enter the Client Id (aka 'Application ID')" = $clientAadApplication.AppId;"Enter the API scopes as declared in the app registration 'Expose an Api' blade in the form of 'api://{client_id}/.default'" = ("api://"+$serviceAadApplication.AppId+"/access_as_user");"Enter the Web APIs base address, e.g. 'https://localhost:44351/api/profile/'" = $serviceAadApplication.HomePage };
+   $dictionary = @{ "Enter the Client Id (aka 'Application ID')" = $clientAadApplication.AppId;"Enter the API scopes as declared in the app registration 'Expose an Api' blade in the form of 'api://{client_id}/.default'" = ("api://"+$serviceAadApplication.AppId+"/access_as_user");"Enter the Web APIs base address, e.g. 'https://localhost:44351/api/profile'" = $serviceAadApplication.HomePage };
    ReplaceInTextFile -configFilePath $configFile -dictionary $dictionary
    Write-Host ""
    Write-Host -ForegroundColor Green "------------------------------------------------------------------------------------------------" 
